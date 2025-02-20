e476352fb28923de87828bb4c84ed56dce847bac
yassine <imaanlaf@gmail.com>
2025-02-20 19:26:38 +0100

    diva

--- ft_printf.c
+++ ft_printf.c
@@ -1,33 +1,43 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <limits.h>
 
-// Function prototypes
+
 void ft_putchar(char c, int *len);
 void ft_putstr(char *str, int *len);
 void ft_putnbr(int n, int *len);
 void ft_putUnbr(unsigned int n, int *len);
-void ft_puthex
+void ft_puthex(unsigned int n, int *len, int uppercase);
 int ft_printf(const char *format, ...);
 
-// Main function
 int main(void)
 {
     char s = 'y';
-    int a = 13;
-    char *str = "hi imane";
-    unsigned int n = 1234;
+    int a = 42;
+    char *str = "DIVA";
+    unsigned int n = 1337;
+    unsigned int i = 255;
 
-    ft_printf("Hi %c\n", s);
-    ft_printf("hi %d\n", a);
-    ft_printf("%s\n", str);
-    ft_printf("%u\n",n);
-    printf("hello %c\n", s);
+    ft_printf("print char :  %c\n", s);
+    ft_printf("print digit : %d\n", a);
+    ft_printf("print string :%s\n", str);
+    ft_printf("print unsigned digit :%u\n",n);
+    ft_printf("print lowercase hexa :%x\n",i);
+    ft_printf("print uppercase hexa :%X\n",i);
+    ft_printf("spicifier: %%\n");
 
-    return 0;
+ 
+    ft_printf("%s\n", ""); // Empty string
+    ft_printf("%s\n", NULL); // NULL string
+    ft_printf("%d\n", 0); // Zero
+    ft_printf("%d\n", -123); // Negative number
+    ft_printf("%u\n", UINT_MAX); // Maximum unsigned integer
+    ft_printf("%x\n", UINT_MAX); // Maximum hexadecimal (lowercase)
+    ft_printf("%X\n", UINT_MAX); // Maximum hexadecimal (uppercase
+	return 0;
 }
 
-// Function definitions
 void ft_putchar(char c, int *len)
 {
     write(1, &c, 1);
@@ -71,6 +81,23 @@ void ft_putUnbr(unsigned int n, int *len)
 	ft_putchar((n % 10)+48, len);
 }
 
+void ft_puthex(unsigned int n, int *len, int uppercase)
+{
+	char *hex_digits;
+	
+	if (uppercase)
+		hex_digits = "0123456789ABCDEF";
+	else
+		hex_digits = "0123456789abcdef";
+	
+	if (n >= 16)
+		
+		ft_puthex(n / 16, len, uppercase); 
+	
+	ft_putchar(hex_digits[n % 16],len);
+	
+}
+
 int ft_printf(const char *format, ...)
 {
     va_list args;
@@ -91,6 +118,10 @@ int ft_printf(const char *format, ...)
                 ft_putnbr(va_arg(args, int), &len);
 	    else if (format[i] == 'u')
 		    ft_putUnbr(va_arg(args, unsigned int),&len);
+	    else if (format[i] == 'x')
+		    ft_puthex(va_arg(args,unsigned int),&len,0);
+	    else if (format[i] == 'X')
+		    ft_puthex(va_arg(args,unsigned int),&len,1);
             else if (format[i] == '%')
                 ft_putchar('%', &len);
         }
